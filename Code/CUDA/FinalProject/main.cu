80f376cc9e63f15fab6c5d77a66c17ccf93074c9
darrin <lukenuculaj@gmail.com>
2024-11-17 17:03:22 -0500

    Finished and tested kernel for Step 4 with m parallel threads on flattened matrices

--- Code/CUDA/FinalProject/main.cu
+++ Code/CUDA/FinalProject/main.cu
@@ -1,18 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #define EPSILON 1e-6
+#define COMP_EPSILON 1e-8
 //#define STEP2
-#define STEP4 
+#define STEP4
+#define num_states 150
 
 __global__ void StepTwoGPADParallelCols(const float* __restrict__ M_G, const float* __restrict__ g_P, float* w_v, float* zhat_v, int N, int n_u, int m){
 	
 	// insert parallel code here
 }
 
-__global__ void StepFourGPADParallelCols(const float* __restrict__ G_L, float* y_vp1, float* w_v, const float* __restrict__ p_D, float* zhat_v, int N, int n_u, int m){
+__global__ void StepFourGPADParRows(const float* __restrict__ G_L, float* y_vp1, float* w_v, const float* __restrict__ p_D, float* zhat_v, const int N, const int n_u, const int m){
 	
-	// insert parallel code here
+	// launch m threads to compute a unique element of the output vector 
+	
+	__shared__ float zhat_vs[num_states]; 
+	int tx = threadIdx.x; 
+	int bx = blockIdx.x; 
+	int index = tx + bx*blockDim.x; 
+	
+	// collaborate in loading the shared memory with m threads
+	int i = 0; 
+	while(tx + i < num_states){
+		zhat_vs[tx + i] = zhat_v[tx + i]; // coalesced memory accesses 
+		i += blockDim.x; 
+	}
+	__syncthreads(); 
+	
+	// handle out of bounds 
+	if(index < m){
+		float sum = 0.0f; 
+		float yraw = 0.0f; 
+		for (int j = 0; j < N; j++){
+			if (index < 4*n_u*N){
+				sum += G_L[index*N + j]*zhat_vs[j*n_u + (index % n_u)]; 
+			}
+			else{
+				for(int k = 0; k < n_u; k++){
+					sum += G_L[index*N + j]*zhat_vs[j*n_u + k]; 
+				}
+			}
+		}
+		yraw = sum + w_v[index] + p_D[index]; // sum 
+		
+		y_vp1[index] = (yraw < COMP_EPSILON) ? 0 : yraw; // projection onto nonnegative orthant
+	}
+
+}
+
+__global__ void StepFourGPADParElements(const float* __restrict__ G_L, float* y_vp1, float* w_v, const float* __restrict__ p_D, float* zhat_v, const int N, const int n_u, const int m){
+	
+	// launch m*N threads to compute a unique element of the output vector 
+	__shared__ float zhat_vs[blockDim.x]; 
+	int tx = threadIdx.x; int ty = threadIdx.y; 
+	int bx = blockIdx.x; int by = blockIdx.y; 
+	int row = by*blockDim.y + ty; 
+	int col = bx*blockDim.x + tx; 
+	
+	// collaborate in loading the shared memory with m threads
+	int i = 0; 
+	while(tx + i < num_states){
+		zhat_vs[tx + i] = zhat_v[tx + i]; // coalesced memory accesses 
+		i += blockDim.x; 
+	}
+	__syncthreads(); 
+	
+	// handle out of bounds 
+	if(index < m){
+		float sum = 0.0f; 
+		float yraw = 0.0f; 
+		for (int j = 0; j < N; j++){
+			if (index < 4*n_u*N){
+				sum += G_L[index*N + j]*zhat_vs[j*n_u + (index % n_u)]; 
+			}
+			else{
+				for(int k = 0; k < n_u; k++){
+					sum += G_L[index*N + j]*zhat_vs[j*n_u + k]; 
+				}
+			}
+		}
+		yraw = sum + w_v[index] + p_D[index]; // sum 
+		
+		y_vp1[index] = (yraw < COMP_EPSILON) ? 0 : yraw; // projection onto nonnegative orthant
+	}
+
+}
+
+__global__ void ParPositiveProjection(float* vec, int size){
+	
+	int index = threadIdx.x + blockDim.x*blockIdx.x; 
+	if (index < size){
+		if (vec[index] < COMP_EPSILON) vec[index] = 0.0f; 
+	}
+	
 }
 
 // Sequential implementation of Step 2
@@ -267,7 +350,45 @@ int main(int argc, char *argv[]){
 	
 	// STEP 4: y_{v+1} <-- [w_v + G_L * zhat_v + p_D]_+ (INSERT HERE)
 	#ifdef STEP4 
-	StepFourGPADSequential(G_L, y_vp1, w_v, p_D, zhat_v, N, n_u, m);
+	
+	// GPU memory allocation and transfer
+	float* dG_L;
+	float* dy_vp1; 
+	float* dw_v; 
+	float* dp_D; 
+	float* dzhat_v; 
+	cudaMalloc((void**)&dG_L, N*m*sizeof(float)); 
+	cudaMalloc((void**)&dy_vp1, m*sizeof(float)); 
+	cudaMalloc((void**)&dw_v, m*sizeof(float)); 
+	cudaMalloc((void**)&dp_D, m*sizeof(float)); 
+	cudaMalloc((void**)&dzhat_v, N*n_u*sizeof(float)); 	
+	cudaMemcpy(dG_L, G_L, N*m*sizeof(float), cudaMemcpyHostToDevice); 
+	cudaMemcpy(dw_v, w_v, m*sizeof(float), cudaMemcpyHostToDevice); 
+	cudaMemcpy(dp_D, p_D, m*sizeof(float), cudaMemcpyHostToDevice); 
+	cudaMemcpy(dzhat_v, zhat_v, N*n_u*sizeof(float), cudaMemcpyHostToDevice); 
+	struct timeval gpu_start, gpu_finish; 
+	struct timeval cpu_start, cpu_finish; 
+	
+	// Grid and block sizes 
+	dim3 bd((int)min(1024.0, (float)m)); 
+	dim3 gd((int)ceil((float)m/1024.0)); 
+	
+	// Kernel launch and synchronize
+	int num_trials = 100; 
+	gettimeofday(&gpu_start, NULL);
+	for (int i = 0; i < num_trials; i++){
+		StepFourGPADParallelRows<<<gd, bd>>>(dG_L, dy_vp1, dw_v, dp_D, dzhat_v, N, n_u, m); 
+		cudaDeviceSynchronize();
+	}
+	gettimeofday(&gpu_finish, NULL);
+	
+	cudaMemcpy(y_vp1, dy_vp1, m*sizeof(float), cudaMemcpyDeviceToHost);
+	
+	gettimeofday(&cpu_start, NULL);
+	for (int i = 0; i < num_trials; i++){
+		StepFourGPADSequential(G_L, y_vp1, w_v, p_D, zhat_v, N, n_u, m);
+	}
+	gettimeofday(&cpu_finish, NULL); 
 	
 	int status_success = 0; 
 	int fail_index = 0; 
@@ -287,6 +408,9 @@ int main(int argc, char *argv[]){
 	else{
 		printf("\nStatus: SUCCESS!\n"); 
 	}
+	
+	printf("Avg. CPU Execution Time = %lu usec\n", (cpu_finish.tv_usec - cpu_start.tv_usec)/num_trials); 
+	printf("Avg. GPU Execution Time = %lu usec\n", (gpu_finish.tv_usec - gpu_start.tv_usec)/num_trials); 
 	
 	#endif 
 	// end algorithm 
@@ -312,6 +436,11 @@ int main(int argc, char *argv[]){
 	free(prod_Gz); 
 	free(exp_prod_Gz); 
 	free(exp_y_vp1);	
+	cudaFree(dG_L); 
+	cudaFree(dy_vp1); 
+	cudaFree(dw_v); 
+	cudaFree(dp_D); 
+	cudaFree(dzhat_v);
 	#endif 
 	
 	return 0; 
